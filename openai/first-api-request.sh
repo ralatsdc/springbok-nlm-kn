f7dd05a388222ded07e84a1b0069f1c3e0d98b47
Raymond LeClair <raymond.leclair@gmail.com>
2024-06-11 18:06:28 -0400

    Add URLs to relevant pages

--- openai/first-api-request.sh
+++ openai/first-api-request.sh
@@ -1,3 +1,6 @@
+# https://platform.openai.com/settings/profile
+# https://platform.openai.com/api-keys
+# https://platform.openai.com/docs/api-reference/introduction
 curl https://api.openai.com/v1/chat/completions \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $OPENAI_API_KEY" \
