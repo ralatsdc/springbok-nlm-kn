b69aca423884ac46874c178e998a66fef94acfb2
Raymond LeClair <raymond.leclair@gmail.com>
2024-06-18 14:41:22 -0400

    Add setup script from openai examples

---
+++ ncbi-cell/setup.sh
@@ -0,0 +1,4 @@
+runoak set-apikey -e bioportal $BIOPORTAL_API_KEY
+runoak set-apikey -e ncbi-email raymond.leclair@gmail.com
+runoak set-apikey -e ncbi-key $NCBI_API_KEY
+runoak set-apikey -e openai $OPENAI_API_KEY
