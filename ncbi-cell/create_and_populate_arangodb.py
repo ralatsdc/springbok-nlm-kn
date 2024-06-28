392db3d34c4df2f71fe2c509e238a7f91ea14037
Raymond LeClair <raymond.leclair@gmail.com>
2024-06-27 21:04:02 -0400

    Add documentation

--- ncbi-cell/create_and_populate_arangodb.py
+++ ncbi-cell/create_and_populate_arangodb.py
@@ -33,7 +33,17 @@ GENE_COLLECTION = "gene"
 
 
 def create_or_get_database():
+    """Create or get NCBI-Cell database.
 
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    db : arango.database.StandardDatabase
+        NCBI-Cell database
+    """
     # Create NCBI-Cell database, if needed
     if not SYS_DB.has_database(NCBI_CELL_DB):
         SYS_DB.create_database(NCBI_CELL_DB)
@@ -45,14 +55,36 @@ def create_or_get_database():
 
 
 def delete_database():
+    """Delete NCBI-Cell database.
 
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
     # Delete NCBI-Cell database, if needed
     if SYS_DB.has_database(NCBI_CELL_DB):
         SYS_DB.delete_database(NCBI_CELL_DB)
 
 
 def create_or_get_graph(db):
+    """Create or get NCBI-Cell database graph.
 
+    Parameters
+    ----------
+    db : arango.database.StandardDatabase
+        NCBI-Cell database
+
+    Returns
+    -------
+    graph : arango.graph.Graph
+        NCBI-Cell database graph
+    db : arango.database.StandardDatabase
+        NCBI-Cell database
+    """
     # Create, or get the graph
     if db.has_graph(NCBI_CELL_GRAPH):
         graph = db.graph(NCBI_CELL_GRAPH)
@@ -62,9 +94,40 @@ def create_or_get_graph(db):
     return graph, db
 
 
-def create_and_populate_or_get_vertex_collection_cellxgene(graph):
+def delete_graph(db):
+    """Delete NCBI-Cell database graph.
 
-    # Create or get the cellxgne vertex collection
+    Parameters
+    ----------
+    db : arango.database.StandardDatabase
+        NCBI-Cell database
+
+    Returns
+    -------
+    """
+    # Delete the graph
+    if db.has_graph(NCBI_CELL_GRAPH):
+        db.delete_graph(NCBI_CELL_GRAPH)
+
+
+def create_and_populate_or_get_vertex_collection_cellxgene(graph):
+    """Create or get the CELLXGENE vertex collection, read the
+    preprocessed datasets parquet, and insert a vertex for each row of
+    the datasets DataFrame.
+
+    Parameters
+    ----------
+    graph : arango.graph.Graph
+        NCBI-Cell database graph
+
+    Returns
+    -------
+    cellxgene : arango.collection.VertexCollection
+        CELLXGENE vertex collection
+    graph : arango.graph.Graph
+        NCBI-Cell database graph
+    """
+    # Create or get the cellxgene vertex collection
     if graph.has_vertex_collection(CELLXGENE_COLLECTION):
         cellxgene = graph.vertex_collection(CELLXGENE_COLLECTION)
     else:
@@ -84,7 +147,21 @@ def create_and_populate_or_get_vertex_collection_cellxgene(graph):
 
 
 def create_and_populate_or_get_vertex_collection_nsforest(graph):
+    """Create or get the NSForest vertex collection, read each
+    NSForest results file, and insert a vertex for results DataFrame.
 
+    Parameters
+    ----------
+    graph : arango.graph.Graph
+        NCBI-Cell database graph
+
+    Returns
+    -------
+    nsforest : arango.collection.VertexCollection
+        NSForest vertex collection
+    graph : arango.graph.Graph
+        NCBI-Cell database graph
+    """
     # Create, or get the nsforest vertex collection
     if graph.has_vertex_collection(NSFOREST_COLLECTION):
         nsforest = graph.vertex_collection(NSFOREST_COLLECTION)
@@ -99,17 +176,31 @@ def create_and_populate_or_get_vertex_collection_nsforest(graph):
         dataset_id = os.path.basename(os.path.dirname(fn))
         df["dataset_id"] = dataset_id
 
-        # Insert documents using clusterName as _key
-        df["_key"] = df["clusterName"]
-        for index, row in df.iterrows():
-            if not nsforest.has(row["_key"]):
-                nsforest.insert(json.loads(row.to_json()))
+        # Insert documents using dataset_id as _key
+        _key = dataset_id
+        if not nsforest.has(_key):
+            d = dataframe_to_doc(df, _key)
+            nsforest.insert(d)
 
     return nsforest, graph
 
 
 def create_and_populate_or_get_vertex_collection_ontogpt(graph):
+    """Create or get the OntoGPT vertex collection, read each OntoGPT
+    output file, and insert a vertex for output object.
 
+    Parameters
+    ----------
+    graph : arango.graph.Graph
+        NCBI-Cell database graph
+
+    Returns
+    -------
+    ontogpt : arango.collection.VertexCollection
+        OntoGPT vertex collection
+    graph : arango.graph.Graph
+        NCBI-Cell database graph
+    """
     # Create, or get the ontogpt vertex collection
     if graph.has_vertex_collection(ONTOGPT_COLLECTION):
         ontogpt = graph.vertex_collection(ONTOGPT_COLLECTION)
@@ -136,7 +227,30 @@ def create_and_populate_or_get_vertex_collection_ontogpt(graph):
 
 
 def create_and_populate_or_get_vertex_collection_cell(graph, nsforest, ontogpt):
+    """Create or get the cell vertex collection, and insert a vertex
+    corresponding to each cell identified in the nsforest and ontogpt
+    vertex collections.
 
+    Parameters
+    ----------
+    graph : arango.graph.Graph
+        NCBI-Cell database graph
+    nsforest : arango.collection.VertexCollection
+        NSForest vertex collection
+    ontogpt : arango.collection.VertexCollection
+        OntoGPT vertex collection
+
+    Returns
+    -------
+    cell : arango.collection.VertexCollection
+        cell vertex collection
+    graph : arango.graph.Graph
+        NCBI-Cell database graph
+    nsforest : arango.collection.VertexCollection
+        NSForest vertex collection
+    ontogpt : arango.collection.VertexCollection
+        OntoGPT vertex collection
+    """
     # Create, or get the cell vertex collection
     if graph.has_vertex_collection(CELL_COLLECTION):
         cell = graph.vertex_collection(CELL_COLLECTION)
@@ -167,33 +281,85 @@ def create_and_populate_or_get_vertex_collection_cell(graph, nsforest, ontogpt):
 
         else:
             gpt["cell_id"] = None
+
+        # Update the ontgpt vertex with the cell_id
         ontogpt.update(gpt)
 
     return cell, graph, nsforest, ontogpt
 
 
 def create_and_populate_or_get_vertex_collection_gene(graph, nsforest):
+    """Create or get the gene vertex collection, and insert a vertex
+    corresponding to each marker for each cell identified in the
+    nsforest vertex collection.
 
+    Parameters
+    ----------
+    graph : arango.graph.Graph
+        NCBI-Cell database graph
+    nsforest : arango.collection.VertexCollection
+        NSForest vertex collection
+
+    Returns
+    -------
+    graph : arango.graph.Graph
+        NCBI-Cell database graph
+    nsforest : arango.collection.VertexCollection
+        NSForest vertex collection
+    """
     # Create, or get the gene vertex collection
     if graph.has_vertex_collection(GENE_COLLECTION):
         gene = graph.vertex_collection(GENE_COLLECTION)
     else:
         gene = graph.create_vertex_collection(GENE_COLLECTION)
 
-    # Subset each document, then insert it using each of
-    # NSForest_markers as _key
+    # Read each nsforest vertex JSON to create a DataFrame
     for nsf in nsforest.all():
-        d = {k: nsf[k] for k in ("clusterName", "dataset_id")}
-        for mrk in ast.literal_eval(nsf["NSForest_markers"]):
-            d["_key"] = mrk
-            if not gene.has(d["_key"]):
-                gene.insert(d)
+        df = doc_to_dataframe(nsf)
+
+        # Consider each row of the DataFrame
+        for index, row in df.iterrows():
+
+            # Consider each marker in the row
+            for mrk in ast.literal_eval(row["NSForest_markers"]):
+                _key = mrk
+
+                # Insert or update a gene vertex using the marker as
+                # key, collecting all clusterNames and dataset_ids
+                # corresponding to the gene vertex
+                if not gene.has(_key):
+                    d = {
+                        "_key": _key,
+                        "clusterNames": [row["clusterName"]],
+                        "dataset_ids": [row["dataset_id"]],
+                    }
+                    gene.insert(d)
+
+                else:
+                    d = gene.get(_key)
+                    d["clusterNames"].append(row["clusterName"])
+                    d["dataset_ids"].append(row["dataset_id"])
+                    gene.update(d)
 
     return gene, graph, nsforest
 
 
 def create_or_get_edge_collection_cellxgene_cell(graph):
+    """Create, or get the from CELLXGENE to cell edge collection.
 
+    Parameters
+    ----------
+    graph : arango.graph.Graph
+        NCBI-Cell database graph
+
+    Returns
+    -------
+    cellxgene_cell : arango.collection.EdgeCollection
+        From CELLXGENE to cell edge collection
+    graph : arango.graph.Graph
+        NCBI-Cell database graph
+    """
+    # Create, or get the from cellxgene to cell edge collection
     if not graph.has_edge_definition(f"{CELLXGENE_COLLECTION}-{CELL_COLLECTION}"):
         cellxgene_cell = graph.create_edge_definition(
             edge_collection=f"{CELLXGENE_COLLECTION}-{CELL_COLLECTION}",
@@ -209,7 +375,21 @@ def create_or_get_edge_collection_cellxgene_cell(graph):
 
 
 def create_or_get_edge_collection_ontogpt_cell(graph):
+    """Create, or get the from OntoGPT to cell edge collection.
 
+    Parameters
+    ----------
+    graph : arango.graph.Graph
+        NCBI-Cell database graph
+
+    Returns
+    -------
+    ontogpt_cell : arango.collection.EdgeCollection
+        From OntoGPT to cell edge collection
+    graph : arango.graph.Graph
+        NCBI-Cell database graph
+    """
+    # Create, or get the from ontogpt to cell edge collection
     if not graph.has_edge_definition(f"{ONTOGPT_COLLECTION}-{CELL_COLLECTION}"):
         ontogpt_cell = graph.create_edge_definition(
             edge_collection=f"{ONTOGPT_COLLECTION}-{CELL_COLLECTION}",
@@ -223,7 +403,21 @@ def create_or_get_edge_collection_ontogpt_cell(graph):
 
 
 def create_or_get_edge_collection_cell_gene(graph):
+    """Create, or get the from cell to gene edge collection.
 
+    Parameters
+    ----------
+    graph : arango.graph.Graph
+        NCBI-Cell database graph
+
+    Returns
+    -------
+    cell_gene : arango.collection.EdgeCollection
+        From cell to gene edge collection
+    graph : arango.graph.Graph
+        NCBI-Cell database graph
+    """
+    # Create, or get the from cell to gene edge collection
     if not graph.has_edge_definition(f"{CELL_COLLECTION}-{GENE_COLLECTION}"):
         cell_gene = graph.create_edge_definition(
             edge_collection=f"{CELL_COLLECTION}-{GENE_COLLECTION}",
@@ -237,13 +431,35 @@ def create_or_get_edge_collection_cell_gene(graph):
 
 
 def insert_cellxgene_cell_edges(cellxgene, cell, cellxgene_cell):
+    """Insert an edge from any CELLXGENE vertex to a cell vertex if
+    they share their dataset_id.
 
+    Parameters
+    ----------
+    cellxgene : arango.collection.VertexCollection
+        CELLXGENE vertex collection
+    cell : arango.collection.VertexCollection
+        cell vertex collection
+    cellxgene_cell : arango.collection.EdgeCollection
+        From CELLXGENE to cell edge collection
+
+    Returns
+    -------
+    cellxgene : arango.collection.VertexCollection
+        CELLXGENE vertex collection
+    cell : arango.collection.VertexCollection
+        cell vertex collection
+    cellxgene_cell : arango.collection.EdgeCollection
+        From CELLXGENE to cell edge collection
+    """
+    # Consider each cellxgene vertex
     for cxg in cellxgene.all():
         cxg_key = cxg["_key"]
         print(
             f"Finding edges to {CELL_COLLECTION} from {CELLXGENE_COLLECTION} document with key: {cxg_key}"
         )
 
+        # Consider each cell vertex
         found = False
         for cll in cell.all():
             if "dataset_id" in cll and cll["dataset_id"] == cxg["dataset_id"]:
@@ -269,7 +485,29 @@ def insert_cellxgene_cell_edges(cellxgene, cell, cellxgene_cell):
 
 
 def insert_ontogpt_cell_edges(ontogpt, cell, ontogpt_cell):
+    """Insert an edge from an OntoGPT vertex to a cell vertex if the
+    OntoGPT vertex cell_id is the cell key. Currently, this cannot
+    happen.
 
+    Parameters
+    ----------
+    ontogpt : arango.collection.VertexCollection
+        OntoGPT vertex collection
+    cell : arango.collection.VertexCollection
+        cell vertex collection
+    ontogpt_cell : arango.collection.EdgeCollection
+        From OntoGPT to cell edge collection
+
+    Returns
+    -------
+    ontogpt : arango.collection.VertexCollection
+        OntoGPT vertex collection
+    cell : arango.collection.VertexCollection
+        cell vertex collection
+    ontogpt_cell : arango.collection.EdgeCollection
+        From OntoGPT to cell edge collection
+    """
+    # Consider each ontogpt vertex
     for gpt in ontogpt.all():
         gpt_key = gpt["_key"]
         print(
@@ -301,7 +539,24 @@ def insert_ontogpt_cell_edges(ontogpt, cell, ontogpt_cell):
 
 
 def insert_cell_gene_edges(nsforest, cell_gene):
+    """Use the NSForest vertex collection to insert an edge from every
+    cell vertex to the marker gene vertex.
 
+    Parameters
+    ----------
+    nsforest : arango.collection.VertexCollection
+        NSForest vertex collection
+    cell_gene : arango.collection.EdgeCollection
+        From cell to gene edge collection
+
+    Returns
+    ----------
+    nsforest : arango.collection.VertexCollection
+        NSForest vertex collection
+    cell_gene : arango.collection.EdgeCollection
+        From cell to gene edge collection
+    """
+    # Read each nsforest vertex JSON to create a DataFrame
     for nsf in nsforest.all():
         cll_key = nsf["clusterName"]
 
@@ -321,16 +576,20 @@ def insert_cell_gene_edges(nsforest, cell_gene):
 
 
 def main():
+    # TODO: Use this function to clean up variables in scope
     pass
 
 
 if __name__ == "__main__":
 
+    # Create a new database
     delete_database()
     db = create_or_get_database()
 
+    # Create a graph
     graph, db = create_or_get_graph(db)
 
+    # Create and populate vertex collections
     cellxgene, graph = create_and_populate_or_get_vertex_collection_cellxgene(graph)
     nsforest, graph = create_and_populate_or_get_vertex_collection_nsforest(graph)
     ontogpt, graph = create_and_populate_or_get_vertex_collection_ontogpt(graph)
@@ -341,10 +600,12 @@ if __name__ == "__main__":
         graph, nsforest
     )
 
+    # Create edge collections
     cellxgene_cell, graph = create_or_get_edge_collection_cellxgene_cell(graph)
     ontogpt_cell, graph = create_or_get_edge_collection_ontogpt_cell(graph)
     cell_gene, graph = create_or_get_edge_collection_cell_gene(graph)
 
+    # Insert edges
     cellxgene, cell, cellxgene_cell = insert_cellxgene_cell_edges(
         cellxgene, cell, cellxgene_cell
     )
