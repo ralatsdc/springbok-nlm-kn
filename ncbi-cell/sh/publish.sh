0489485467caa3ce4bef7d0efc4403332b06a8da
Raymond LeClair <raymond.leclair@gmail.com>
2024-07-21 12:56:45 +0200

    Make runnable anywhere, and document (slightly)

--- ncbi-cell/sh/publish.sh
+++ ncbi-cell/sh/publish.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
-org_files=$(ls *.org)
+# Assign home directory of this script
+home=$(dirname $(dirname $(realpath $0)))
+
+# Tangle, and convert to .ipynb eac Org mode file
+org_files=$(ls $home/org/*.org)
 for org_file in $org_files; do
-    ipynb_file=$(echo $org_file | sed s/.org/.ipynb/)
+    ipynb_file=$(basename $org_file | sed s/.org/.ipynb/)
 
     echo "Tangling $org_file"
     emacs --batch --eval "(require 'org)" --eval "(org-babel-tangle-file \"$org_file\")"
 
     echo "Converting $org_file to $ipynb_file"
     cat $org_file | sed 's/src python/src jupyter-python/' > tmp.org
-    pandoc tmp.org -o $ipynb_file
+    pandoc tmp.org -o $home/ipynb/$ipynb_file
     rm tmp.org
-    
+
 done
 
-black *.py
+# Blacken all Python files
+black $home/py/*.py
