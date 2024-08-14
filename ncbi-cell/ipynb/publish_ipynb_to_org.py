183498ca633306a37f525d522f97cc3a1573ae1a
Raymond LeClair <raymond.leclair@gmail.com>
2024-08-14 16:45:36 -0400

    Add script to publishe Jupyter notebooks to Org mode files

---
+++ ncbi-cell/ipynb/publish_ipynb_to_org.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+import json
+from pathlib import Path
+import re
+
+
+def write_markdown(fp, cell):
+    """Write a Markdown cell.
+
+    Parameters
+    ----------
+    fp : _io.TextIOWrapper
+        File pointer
+    cell : dict
+        The Markdown cell dictionary
+
+    Returns
+    -------
+    None
+    """
+    # Write each line
+    spcs = ""
+    for line in cell["source"]:
+
+        # Substitue paired double asterisks with paired single
+        # asterisks
+        m = re.search(r"(\*\*).*(\*\*)", line)
+        if m:
+            line = re.sub(r"(\*\*).*(\*\*)", m.group(0).replace("**", "*"), line)
+
+        # Remove backslash escapes from paired left and right angle
+        # brackets
+        m = re.search(r"(\\\<).*(\\\>)", line)
+        if m:
+            line = re.sub(r"(\\\<).*(\\\>)", m.group(0).replace("\\", ""), line)
+
+        # Replace leading sharps with asterisks
+        m = re.match(r"(#+)(?!\+)", line)
+        if m:
+            line = re.sub(r"(^#+)(?!\+)", m.group(1).replace("#", "*"), line)
+
+        # Replace Markdown with Emacs Org mode link format. Note that
+        # order of link and description is reversed, and "file"
+        # protocol needs to be added.
+        m = re.search(r"(\[.*\])(\(.*?\))", line)
+        if m:
+            link = m.group(2).replace("(", "[").replace(")", "]")
+            if not re.match(r"\[http", link):
+                link = link.replace("[", "[file:")
+            desc = m.group(1)
+            line = re.sub(
+                r"(\[.*\])(\(.*?\))",
+                f"[{link}{desc}]",
+                line,
+            )
+
+        # Replace three space after leading hyphen with one
+        line = re.sub(r"^-   ", "- ", line)
+
+        # Handle emacs-lisp source blocks
+        if "``` commonlisp" in line:
+            fp.write("#+begin_src emacs-lisp :session shared :results silent\n")
+            spcs = "  "
+            continue
+        elif "```" in line:
+            fp.write("#+end_src\n")
+            spcs = ""
+            continue
+
+        # Substitue paired backticks with paired tildes
+        m = re.search(r"(`).*(`)", line)
+        if m:
+            line = re.sub(r"(`).*(`)", m.group(0).replace("`", "~"), line)
+
+        fp.write(spcs + line)
+
+    fp.write("\n\n")
+
+
+def write_code(fp, cell):
+    """Write a code cell.
+
+    Parameters
+    ----------
+    fp : _io.TextIOWrapper
+        File pointer
+    cell : dict
+        The code cell dictionary
+
+    Returns
+    -------
+    None
+    """
+    # Create, then write, begin source line using metadata to populate
+    # options
+    begin_src_str = "#+begin_src python"
+    for k, v in cell["metadata"].items():
+        begin_src_str += f" :{k} {v}"
+    fp.write(begin_src_str + "\n")
+
+    # Write source lines
+    spcs = "  "
+    for line in cell["source"]:
+        if re.fullmatch(r"\s*", line):
+            fp.write(line)
+        else:
+            fp.write(spcs + line)
+
+    # Write end source line
+    fp.write("#+end_src\n\n")
+
+
+def publish_ipynb_to_org(ipynb_path):
+    """Load the specified Jupyter notebook, and write the contents as
+    an Org mode file.
+
+    Parameters
+    ----------
+    ipynb_filename : Path
+       Path of the Jupyter notebook file
+
+    Returns
+    -------
+    None
+    """
+    # Load Jupyter notebook file
+    with open(ipynb_path, "r") as fp:
+        data = json.load(fp)
+
+    # Write Org mode file
+    org_path = ipynb_path.parents[1] / "org" / ipynb_path.name.replace(".ipynb", ".org")
+    with open(org_path, "w") as fp:
+        for cell in data["cells"]:
+            if cell["cell_type"] == "markdown":
+                write_markdown(fp, cell)
+            elif cell["cell_type"] == "code":
+                write_code(fp, cell)
+
+
+def main():
+    """Publish each Jupyter notebook in the directory containing this
+    script as an Org mode file.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
+    ipynb_home = Path(__file__).parent.resolve()
+    for ipynb_path in ipynb_home.glob("*.ipynb"):
+        publish_ipynb_to_org(ipynb_path)
+
+
+if __name__ == "__main__":
+    main()
