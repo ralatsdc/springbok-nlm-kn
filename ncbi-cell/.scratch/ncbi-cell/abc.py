e4a4567d78197dba70dbdbabefe61451bb740c77
Raymond LeClair <raymond.leclair@gmail.com>
2024-10-14 15:40:54 -0400

    Commit draft scratch code (whoa)

--- ncbi-cell/.scratch/ncbi-cell/abc.py
+++ ncbi-cell/.scratch/ncbi-cell/abc.py
@@ -1,22 +1,22 @@
+print("a")
+# import cellxgene_census
+# import numpy as np
+# import pandas as pd
+# import scanpy as sc
 
-import cellxgene_census
-import numpy as np
-import pandas as pd
-import scanpy as sc
+# census = cellxgene_census.open_soma()
 
-census = cellxgene_census.open_soma()
+# census_summary = census["census_info"]["summary"].read().concat().to_pandas()
 
-census_summary = census["census_info"]["summary"].read().concat().to_pandas()
+# census_datasets = census["census_info"]["datasets"].read().concat().to_pandas()
+# census_datasets = census_datasets.set_index("dataset_id")
 
-census_datasets = census["census_info"]["datasets"].read().concat().to_pandas()
-census_datasets = census_datasets.set_index("dataset_id")
+# census_counts = census["census_info"]["summary_cell_counts"].read().concat().to_pandas()
 
-census_counts = census["census_info"]["summary_cell_counts"].read().concat().to_pandas()
+# lung_obs = cellxgene_census.get_obs(
+#     census, "homo_sapiens", value_filter="tissue_general == 'lung' and is_primary_data == True"
+# )
 
-lung_obs = cellxgene_census.get_obs(
-    census, "homo_sapiens", value_filter="tissue_general == 'lung' and is_primary_data == True"
-)
-
-lung_var = cellxgene_census.get_var(
-    census, "homo_sapiens", value_filter="tissue_general == 'lung' and is_primary_data == True"
-)
+# lung_var = cellxgene_census.get_var(
+#     census, "homo_sapiens", value_filter="tissue_general == 'lung' and is_primary_data == True"
+# )
