560a1ed37bbf10fd4429ee577ff6a495180ace5d
Raymond LeClair <raymond.leclair@gmail.com>
2024-10-24 11:08:47 -0400

    Add work in progress

--- nlm-kn/.scratch/ncbi-cell/collect_dataset_qc_metrics.py
+++ nlm-kn/.scratch/ncbi-cell/collect_dataset_qc_metrics.py
@@ -3,12 +3,35 @@ import numpy as np
 import pandas as pd
 import scanpy as sc
 
-census = cellxgene_census.open_soma()
+census = cellxgene_census.open_soma(census_version="2024-07-01")
 
+
+# In Ajith's spreadsheet
+#
+# From datasets
+#     collection_id
+#     collection_name
+#     dataset_title
+#     dataset_h5ad_path
+#
+# From obs
+#     dataset_id
+#     > assay
+#     is_primary_data 
+#     > tissue
+#
+# From counts
+#     organism
+#
+# Added manually
+#     cluster_header
+#     status
 
 # A data frame with high-level information of this Census, e.g. build date, total cell count, etc.
 # Index(['soma_joinid', 'label', 'value'], dtype='object')
 summary = census["census_info"]["summary"].read().concat().to_pandas()
+
+
 
 # A data frame with all datasets from CELLxGENE Discover used to create the Census.
 # Index(['soma_joinid', 'citation', 'collection_id', 'collection_name',
@@ -18,18 +41,22 @@ summary = census["census_info"]["summary"].read().concat().to_pandas()
 datasets = census["census_info"]["datasets"].read().concat().to_pandas()
 datasets = datasets.set_index("dataset_id")
 
+
+
 # A data frame with cell counts stratified by relevant cell metadata
 # Index(['soma_joinid', 'organism', 'category', 'label', 'ontology_term_id',
 #        'total_cell_count', 'unique_cell_count'],
 #       dtype='object')
 counts = census["census_info"]["summary_cell_counts"].read().concat().to_pandas()
 
+
+
 # Data matrices, currently only raw counts exist X["raw"]
 # X = census["census_data"]["homo_sapiens"].ms["RNA"].X
-adata = cellxgene_census.get_anndata(
-    census=census,
-    organism="Homo sapiens",
-)
+# adata = cellxgene_census.get_anndata(
+#     census=census,
+#     organism="Homo sapiens",
+# )
 
 # Cell metadata
 # obs = census["census_data"]["homo_sapiens"].obs
@@ -44,14 +71,43 @@ adata = cellxgene_census.get_anndata(
 #        'tissue_general_ontology_term_id', 'raw_sum', 'nnz', 'raw_mean_nnz',
 #        'raw_variance_nnz', 'n_measured_vars'],
 #       dtype='object')
-obs = cellxgene_census.get_obs(census, "homo_sapiens")
+#
+# Get the observation metadata for a query on the census.
+#
+# Parameters:
+# census – The census object, usually returned by open_soma().
+# organism – The organism to query, usually one of "Homo sapiens or "Mus musculus"
+# value_filter=None – Value filter for the obs metadata. Value is a filter query written in the SOMA value_filter syntax.
+# coords=None – Coordinates for the obs axis, which is indexed by the soma_joinid value. May be an int, a list of int, or a slice. The default, None, selects all.
+# column_names=None – Columns to fetch.
+#
+# Returns:
+# A pandas.DataFrame object containing metadata for the queried slice.
+obs_hs = cellxgene_census.get_obs(census, "homo_sapiens")
+obs_mm = cellxgene_census.get_obs(census, "mus_musculus")
+
+
 
 # Gene Metadata
 # var = census["census_data"]["homo_sapiens"].ms["RNA"].var
 # Index(['soma_joinid', 'feature_id', 'feature_name', 'feature_length', 'nnz',
 #        'n_measured_obs'],
 #       dtype='object')
+#
+# Get the variable metadata for a query on the census.
+#
+# Parameters:
+# census – The census object, usually returned by open_soma().
+# organism – The organism to query, usually one of "Homo sapiens or "Mus musculus"
+# value_filter – Value filter for the var metadata. Value is a filter query written in the SOMA value_filter syntax.
+# coords – Coordinates for the var axis, which is indexed by the soma_joinid value. May be an int, a list of int, or a slice. The default, None, selects all.
+# column_names – Columns to fetch.
+#
+# Returns:
+# A pandas.DataFrame object containing metadata for the queried slice.
 var = cellxgene_census.get_var(census, "homo_sapiens")
+
+
 
 # See: https://chanzuckerberg.github.io/cellxgene-census/notebooks/api_demo/census_query_extract.html
 
@@ -64,6 +120,11 @@ var = cellxgene_census.get_var(census, "homo_sapiens")
 #     var_value_filter="feature_id in ['ENSG00000161798', 'ENSG00000188229']",
 #     obs_value_filter="cell_type == 'B cell' and tissue_general == 'lung' and disease == 'COVID-19' and is_primary_data == True",
 #     obs_column_names=["sex"],
+# )
+# adata = cellxgene_census.get_anndata(
+#     census=census,
+#     organism="Homo sapiens",
+#     obs_value_filter="tissue_general == 'lung' and is_primary_data == True",
 # )
 
 # Get the observation metadata for a query on the census.
