60314aa595cde8206ebe20b15982277834d93acd
Raymond LeClair <raymond.leclair@gmail.com>
2024-11-06 11:49:51 -0500

    Refactor for clarity

--- nlm-kn/py/HlcaCellRef.py
+++ nlm-kn/py/HlcaCellRef.py
@@ -1,3 +1,4 @@
+import argparse
 import ast
 from pathlib import Path
 import random
@@ -9,92 +10,101 @@ import pandas as pd
 from CellOntology import parse_term
 
 
-alphabet = string.ascii_lowercase + string.digits
+ALPHABET = string.ascii_lowercase + string.digits
 
 
 def get_uuid():
-    return "".join(random.choices(alphabet, k=8))
+    return "".join(random.choices(ALPHABET, k=8))
 
 
-data_dir = Path("/Users/raymondleclair/Projects/NLM/NLM-KN/cell2Knowledge/data")
-data_fnm = "HLCA_CellRef_matching_ver3_import1.xlsm"
-data = pd.read_excel(data_dir / data_fnm, header=1, skiprows=0)
-data["uuid"] = [get_uuid() for idx in data.index]
+def load_data(data_dir, data_fnm):
+    data = pd.read_excel(data_dir / data_fnm, header=1, skiprows=0)
+    data["uuid"] = [get_uuid() for idx in data.index]
+    return data
 
 
-db_name = "hlca_cellref"
-graph_name = "hlca_cellref"
-
-db_name = "BioPortal-Slim"
-graph_name = "CL-Slim"
-
-# adb.delete_database(db_name)
-db = adb.create_or_get_database(db_name)
-adb_graph = adb.create_or_get_graph(db, graph_name)
+def get_graph(db_name, graph_name):
+    db = adb.create_or_get_database(db_name)
+    adb_graph = adb.create_or_get_graph(db, graph_name)
+    return adb_graph
 
 
-vertex_collections = {}
-vertex_names = [
-    "anatomic_structure",
-    "biomarker_combination",
-    "cell_set",
-    "CL",
-    "gene",
-    "publication",
-]
-for vertex_name in vertex_names:
-    collection = adb.create_or_get_vertex_collection(adb_graph, vertex_name)
-    vertex_collections[vertex_name] = collection
+def init_collections(adb_graph):
+
+    vertex_collections = {}
+    vertex_names = [
+        "anatomic_structure",
+        "biomarker_combination",
+        "cell_set",
+        "CL",
+        "gene",
+        "publication",
+    ]
+    for vertex_name in vertex_names:
+        collection = adb.create_or_get_vertex_collection(adb_graph, vertex_name)
+        vertex_collections[vertex_name] = collection
+
+    edge_collections = {}
+    vertex_name_pairs = [
+        ("biomarker_combination", "cell_set"),
+        ("CL", "anatomic_structure"),
+        ("cell_set", "CL"),
+        ("cell_set", "gene"),
+        ("cell_set", "publication"),
+        ("CL", "CL"),
+        ("gene", "biomarker_combination"),
+    ]
+    for vertex_name_pair in vertex_name_pairs:
+        from_vertex = vertex_name_pair[0]
+        to_vertex = vertex_name_pair[1]
+        collection, edge_name = adb.create_or_get_edge_collection(
+            adb_graph, from_vertex, to_vertex
+        )
+        edge_collections[edge_name] = collection
+
+    return vertex_collections, edge_collections
 
 
-edge_collections = {}
-vertex_name_pairs = [
-    ("biomarker_combination", "cell_set"),
-    ("CL", "anatomic_structure"),
-    ("cell_set", "CL"),
-    ("cell_set", "gene"),
-    ("cell_set", "publication"),
-    ("CL", "CL"),
-    ("gene", "biomarker_combination"),
-]
-for vertex_name_pair in vertex_name_pairs:
-    from_vertex = vertex_name_pair[0]
-    to_vertex = vertex_name_pair[1]
-    collection, edge_name = adb.create_or_get_edge_collection(
-        adb_graph, from_vertex, to_vertex
+def add_vertices(vertex_collections):
+
+    # Anatomic structures vertex
+    _key = "Organ_12345"
+    if not vertex_collections["anatomic_structure"].has(_key):
+        vertex_collections["anatomic_structure"].insert({"_key": _key, "name": "lung"})
+    anatomic_structure_vertex = vertex_collections["anatomic_structure"].get(_key)
+
+    # Publication vertices
+    _key = "HLCA_2023_Sikkema"
+    if not vertex_collections["publication"].has(_key):
+        vertex_collections["publication"].insert(
+            {
+                "_key": _key,
+                "name": "HLCA_2023_Sikkema_doi.org/10.1038/s41591-023-02327-2",
+            }
+        )
+    publication_hlca_vertex = vertex_collections["publication"].get(_key)
+    _key = "cellRef_2023_Guo"
+    if not vertex_collections["publication"].has(_key):
+        vertex_collections["publication"].insert(
+            {
+                "_key": _key,
+                "name": "cellRef_2023_Guo_doi.org/10.1038/s41467-023-40173-5",
+            }
+        )
+    publication_cellref_vertex = vertex_collections["publication"].get(_key)
+
+    return (
+        anatomic_structure_vertex,
+        publication_hlca_vertex,
+        publication_cellref_vertex,
     )
-    edge_collections[edge_name] = collection
 
 
-# Anatomic structures vertex
-_key = "Organ_12345"
-if not vertex_collections["anatomic_structure"].has(_key):
-    vertex_collections["anatomic_structure"].insert({"_key": _key, "name": "lung"})
-anatomic_structure_vertex = vertex_collections["anatomic_structure"].get(_key)
+def add_vertices_and_edges_from_row(row, vertex_collections, edge_collections):
 
-
-# Publication vertices
-_key = "HLCA_2023_Sikkema"
-if not vertex_collections["publication"].has(_key):
-    vertex_collections["publication"].insert(
-        {
-            "_key": _key,
-            "name": "HLCA_2023_Sikkema_doi.org/10.1038/s41591-023-02327-2",
-        }
+    anatomic_structure_vertex, publication_hlca_vertex, publication_cellref_vertex = (
+        add_vertices(vertex_collections)
     )
-publication_hlca_vertex = vertex_collections["publication"].get(_key)
-_key = "cellRef_2023_Guo"
-if not vertex_collections["publication"].has(_key):
-    vertex_collections["publication"].insert(
-        {
-            "_key": _key,
-            "name": "cellRef_2023_Guo_doi.org/10.1038/s41467-023-40173-5",
-        }
-    )
-publication_cellref_vertex = vertex_collections["publication"].get(_key)
-
-
-for _, row in data.iterrows():
 
     # Biomarker combination vertices
     hlca_genes = []
@@ -267,7 +277,10 @@ for _, row in data.iterrows():
     )
 
     # Cell set IS_INSTANCE cell type triples
-    if row["predicate_HLCA"] == "skos:exactMatch" or row["predicate_HLCA"] == "skos:relatedMatch":
+    if (
+        row["predicate_HLCA"] == "skos:exactMatch"
+        or row["predicate_HLCA"] == "skos:relatedMatch"
+    ):
         triples.extend(
             [
                 # (cell_set_hlca_vertex, {"name": "IS_INSTANCE"}, cell_type_hlca_vertex),
@@ -343,3 +356,57 @@ for _, row in data.iterrows():
 
         if not edge_collections[edge_name].has(edge):
             edge_collections[edge_name].insert(edge)
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Load Manually Curated Data")
+    parser.add_argument(
+        "--data-dirname",
+        default=Path("../data/nlm-kn"),
+        help="Name of directory containing manually curated data",
+    )
+    parser.add_argument(
+        "--data-filename",
+        default="HLCA_CellRef_matching_ver3_import1.xlsm",
+        help="Name of file containing manually curated data",
+    )
+    group = parser.add_argument_group(
+        "Cell Ontology (CL)", "Assume this version of the CL has been loaded"
+    )
+    exclusive_group = group.add_mutually_exclusive_group(required=True)
+    exclusive_group.add_argument(
+        "--slim", action="store_true", help="Assume the slim ontology has been loaded"
+    )
+    exclusive_group.add_argument(
+        "--full", action="store_true", help="Assume the full ontology has been loaded"
+    )
+    group.add_argument(
+        "--include-bnodes",
+        action="store_true",
+        help="Assume BNodes were included when loading",
+    )
+
+    args = parser.parse_args()
+
+    if args.slim:
+        db_name = "BioPortal-Slim"
+        graph_name = "CL-Slim"
+
+    if args.full:
+        db_name = "BioPortal-Full"
+        graph_name = "CL-Full"
+
+    if args.include_bnodes:
+        db_name += "-BNodes"
+        graph_name += "-BNodes"
+
+    data = load_data(args.data_dirname, args.data_filename)
+
+    adb_graph = get_graph(args.db_name, args.graph_name)
+
+    vertex_collections, edge_collections = init_collections(adb_graph)
+
+    for _, row in data.iterrows():
+
+        add_vertices_and_edges_from_row(row, vertex_collections, edge_collections)
