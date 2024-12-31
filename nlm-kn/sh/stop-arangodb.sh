cce8a507cad0bfd6e6715dc625ee0266c79969fc
Raymond LeClair <raymond.leclair@gmail.com>
2024-12-31 10:46:34 -0500

    Add tests for the ArangoDB module

--- nlm-kn/sh/stop-arangodb.sh
+++ nlm-kn/sh/stop-arangodb.sh
@@ -1,2 +1,6 @@
-docker container stop \
-       $(docker ps | grep arangodb | cut -d " " -f 1)
+#!/usr/bin/env bash
+container_id=$(docker ps | grep arangodb | cut -d " " -f 1)
+if [ -n "$container_id" ]; then
+   docker container stop $container_id > /dev/null
+   docker container rm $container_id > /dev/null
+fi
